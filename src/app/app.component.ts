b6d895d7b9db2c0db03efa4b10c0dd12f43ab0f1
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2024-04-18 20:10:08 -0300

    tela de gerenciamento de etapas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EtapaComponent } from "./etapa/etapa.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, EtapaComponent]
 })
 export class AppComponent {
   title = 'tela-etapas';
